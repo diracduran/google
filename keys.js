081d232f4e83a9f0c719d23a0dc587d3986c5a5f
dirac <78232235+diracduran@users.noreply.github.com>
2022-04-13 12:22:48 +0300

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,5 +1,5 @@
 // api key - https://developers.google.com/custom-search/v1/using_rest
-export const API_KEY = 'AIzaSyBPwYNjO4-XlV8uvEoTumad0GG6FjyArNw'; 
+export const API_KEY = 'api key'; 
 
 // context key - https://cse.google.com/cse/create/new
-export const CONTEXT_KEY = 'de90e8b3914626b8e';
+export const CONTEXT_KEY = 'context key';
